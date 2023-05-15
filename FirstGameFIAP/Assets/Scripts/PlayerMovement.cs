6b507b67391c2b356e8789da37ef6e3ce2ffdf85
Pedro Policello <88664809+PedroPolicello@users.noreply.github.com>
2023-05-15 20:57:57 -0300

    Player Movement, Package Import, Courrotine

---
+++ FirstGameFIAP/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Transform playerTransform;
+    private Rigidbody2D playerRig;
+    private bool isOnGround = true;
+
+    [Header("Player Movement")]
+    [SerializeField] private int speed;
+    [SerializeField] private int jumpForce;
+
+    void Awake()
+    {
+        playerRig = GetComponent<Rigidbody2D>();
+        playerTransform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+        playerTransform.Translate(moveX, 0, 0);
+
+        if (Input.GetButtonDown("Jump") && isOnGround)
+        {
+            isOnGround = false;
+            playerRig.AddForce(new Vector2(0, 1) * jumpForce);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOnGround = true;
+
+        if(collision.gameObject.tag == "Enemy")
+        {
+            StartCoroutine(DestroyPlayer());
+        }
+    }
+
+    IEnumerator DestroyPlayer()
+    {
+        Color c = GetComponent<Renderer>().material.color;
+        for (float alpha = 1f; alpha >=0; alpha -= 0.1f)
+        {
+            c.a = alpha;
+            GetComponent<Renderer>().material.color = c;
+            yield return new WaitForSeconds(.1f);
+        }
+        Destroy(gameObject);
+    }
+
+}
